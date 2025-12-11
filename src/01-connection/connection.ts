e2d27f10bdf425bd46ddac11b41a027d7c54a966
Fabiano Parreira <fabiano_the_beast@hotmail.com>
2025-12-11 15:40:12 -0300

    first commit with connection and channel

---
+++ src/01-connection/connection.ts
@@ -0,0 +1,25 @@
+import * as amqp from 'amqplib';
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+async function connect() {
+  try {
+    const connection = await amqp.connect('amqp://user:password@localhost:5672');
+    console.log('Connected to RabbitMQ');
+
+    const channel = await connection.createChannel();
+    console.log('Channel created');
+
+    await sleep(30000); // Simulate some operations
+
+    await channel.close();
+    await connection.close();
+
+    console.log('Connection closed');
+  } catch (error) {
+    console.error('Error connecting to RabbitMQ:', error);
+    throw error;
+  }
+}
+
+connect();
